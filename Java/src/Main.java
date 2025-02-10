6914a2d96e69d5b610d26493731ff0859ccfaa36
Aliaksandr Maiseyeu <a.maiseyeu1.23@gmail.com>
2025-02-10 19:58:05 +0300

    Fibonachhi Number

---
+++ Java/src/Main.java
@@ -0,0 +1,12 @@
+import javax.print.attribute.standard.Fidelity;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+      //  Lab7.MyPrint();
+
+      //  Lab7pr2.PrintIterator();
+
+        //Lab7pr3.Exampl3();
+        Lab1Fibonachhi.PrintFibonachhiNumb(9);
+    }
+}
